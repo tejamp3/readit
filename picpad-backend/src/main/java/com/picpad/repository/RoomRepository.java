c893c204977f685c04c3522bf09f375012d8261a
tejamp3 <animator.bhanu@gmail.com>
2026-03-26 01:07:44 +0530

    chore: remove unwanted folders, keep only frontend and readit-backend

--- picpad-backend/src/main/java/com/picpad/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.picpad.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.picpad.entity.Room;
-
-public interface RoomRepository extends JpaRepository<Room, String> {
-}
